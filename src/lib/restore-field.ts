fa5456014de8b09539c0a58712854eeae2173d61
Anton Kosykh <kelin2025@yandex.ru>
2022-07-01 23:24:47 +0000

    feat: Added `fn` to `restoreField`

--- src/lib/restore-field.ts
+++ src/lib/restore-field.ts
@@ -2,21 +2,34 @@ import { Event, sample } from "effector";
 
 import { Field, Kind } from "../types";
 
-export const restoreField = <T>(params: { field: Field<T>; trigger: Event<T> }) => {
-  const { field, trigger } = params;
+/**
+ * Reduces the amount of updates when using `.restored()`
+ * by applying updates directly to stores instead of
+ * using `sample({ clock, target: field. restored })`
+ * */
+export const restoreField = <T, U>(params: {
+  field: Field<T>;
+  trigger: Event<U>;
+  fn?: (value: U) => T;
+}) => {
+  // @ts-expect-error
+  const { field, trigger, fn = (x: U): T => x } = params;
 
   if (field.kind === Kind.FIELD) {
     field.$value.on(trigger, (_prev, value) => {
-      return value === undefined ? field.$value.defaultState : value;
+      const mappedValue = fn(value);
+      return mappedValue === undefined ? field.$value.defaultState : mappedValue;
     });
     field.$isDirty.on(trigger, () => false);
     sample({
       clock: trigger,
+      fn,
       target: field.restored,
     });
   } else {
     sample({
       clock: trigger,
+      fn,
       target: field.restored,
     });
   }

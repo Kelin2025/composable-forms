51765083b5bfd66e00b00e909493e77b1973a1b7
Anton Kosykh <kelin2025@yandex.ru>
2022-07-01 21:24:32 +0000

    fix: Predicates in conditional fields now have priority in update queue

--- src/forms/conditionalField.ts
+++ src/forms/conditionalField.ts
@@ -3,44 +3,58 @@ import { Store, combine, createEvent, sample } from "effector";
 import { Field, Kind } from "../types";
 import { NO_ERRORS, createErrorsMeta } from "../core";
 
+type Case<T> = [Store<T>, T | ((value: T) => boolean), Field<T>]
+
 /**
  * Returns first field that satisfied a specific condition
  */
-export const conditionalField = <T extends [Store<boolean>, Field<any>][]>(params: {
+export const conditionalField = <T extends Case<any>[]>(params: {
   cases: T;
 }) => {
   // TODO: No restore for conditionalField?
   const restored = createEvent<T[number][1]["$value"]>();
 
+  const mappedCases = params.cases.map(
+    ([
+      source,
+      predicate,
+      {
+        $value,
+        $isDirty,
+        $errors,
+        $isValid,
+        $hasErrors,
+        $dirtyErrors,
+        $isDirtyAndValid,
+        $hasDirtyErrors,
+        kind
+      },
+    ]) => {
+      return ({
+        source,
+        predicate,
+        filter: source.map(value => {
+          return typeof predicate === 'function' ? predicate(value) : value === predicate;
+        }),
+        value: $value,
+        isDirty: $isDirty,
+        errors: $errors,
+        isValid: $isValid,
+        hasErrors: $hasErrors,
+        dirtyErrors: $dirtyErrors,
+        isDirtyAndValid: $isDirtyAndValid,
+        hasDirtyErrors: $hasDirtyErrors,
+        kind
+      });
+    }
+  )
+
+
   // TODO: Solve this hell somehow
   const $combinedCases = combine(
-    ...params.cases.map(
-      ([
-        filter,
-        {
-          $value,
-          $isDirty,
-          $errors,
-          $isValid,
-          $hasErrors,
-          $dirtyErrors,
-          $isDirtyAndValid,
-          $hasDirtyErrors,
-        },
-      ]) =>
-        combine({
-          filter,
-          value: $value,
-          isDirty: $isDirty,
-          errors: $errors,
-          isValid: $isValid,
-          hasErrors: $hasErrors,
-          dirtyErrors: $dirtyErrors,
-          isDirtyAndValid: $isDirtyAndValid,
-          hasDirtyErrors: $hasDirtyErrors,
-        })
-    )
+    ...mappedCases.map(curCase => combine(curCase))
   );
+
   const $validCase = $combinedCases.map((fieldCases) => {
     for (const field of fieldCases) {
       if (field.filter) {
@@ -58,16 +72,37 @@ export const conditionalField = <T extends [Store<boolean>, Field<any>][]>(param
 
   meta.$isDirty.on($validCase, (_prev, field) => (field ? field.isDirty : false));
 
-  for (const fieldCase of params.cases) {
-    sample({
+  const fieldPredicateCases = mappedCases.filter(sourceCase => {
+    return mappedCases.some(fieldCase => {
+      // TODO: Add check for field Kind
+      return fieldCase.value === sourceCase.source
+    })
+  })
+
+  const prioritizedCases = [...mappedCases].sort((a, b) => {
+    if (fieldPredicateCases.includes(a)) {
+      return -1
+    }
+    if (fieldPredicateCases.includes(b)) {
+      return -1
+    }
+    return 0
+  })
+
+  for (const fieldCase of prioritizedCases) {
+    const currentCaseRestored = sample({
       clock: restored,
-      filter: fieldCase[0],
-      target: fieldCase[1].restored,
+      filter: fieldCase.filter,
     });
+    
+    fieldCase.value.on(currentCaseRestored, (_prev, value) => {
+      return value === undefined ? fieldCase.value.defaultState : value
+    })
+    fieldCase.isDirty.on(currentCaseRestored, () => false)
   }
 
   return {
-    $value: $value as T[number][1]["$value"],
+    $value: $value as T[number][2]["$value"],
     restored,
     $errors,
     ...meta,
